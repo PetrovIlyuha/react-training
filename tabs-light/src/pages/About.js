b15b88e9671767dd74cba8738f9a1eabc4ef5a82
PetrovIlyuha <dnamix1@gmail.com>
2020-08-16 15:20:01 +0300

    simple presentational components to fill the tabs that are to be highlighted in their heading

---
+++ tabs-light/src/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div>
+      About Page{" "}
+      <span role="img" aria-label="about page">
+        📚
+      </span>{" "}
+    </div>
+  );
+}
---
+++ tabs-light/src/pages/Features.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Features() {
+  return (
+    <div>
+      Features Page{" "}
+      <span role="img" aria-label="features">
+        🧸
+      </span>
+    </div>
+  );
+}
